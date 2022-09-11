efb7a4e80195c37069a0726ff3eb618a12d1507b
fcmirfan122 <fcmirfan122@gmail.com>
2022-09-11 19:59:23 +0700

    create database migration for aspirations,users,categories and feedback

---
+++ database/migrations/2022_09_11_115734_create_aspirations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('aspirations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('senders_id')->references('id')->on('senders');
+            $table->foreignId('categories_id')->references('id')->on('categories');
+            $table->integer('status');
+            $table->string('title');
+            $table->text('aspiration');
+            $table->text('location');
+            $table->text('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('aspirations');
+    }
+};
